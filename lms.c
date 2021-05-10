e2fd53d7db04bd310429c06f6f161190c2d572f4
Khalid Hasan <anik038opo@gmail.com>
2021-05-11 03:38:50 +0600

    a bit mod

--- lms.c
+++ lms.c
@@ -1,21 +1,19 @@
 #include<stdio.h>
+
 #include<string.h>
-void add();
-void display();
-void author();
-void title();
-void count();
-void access();
+
 struct book
 {
     int a; //a=accession number
     char name[30],title[30],flag[6];
     float price;
 } b[100];
+
 int d;
+
 void main()
 {
-    int i,n,c;
+    /*int i,n,c;
     printf("Enter number of book\n");
     scanf("%d",&n);
     for(i=0; i<n; i++)
@@ -30,41 +28,54 @@ void main()
         printf("Price ");
         scanf("%f",&b[i].price);
         printf("Issue status yes/no: ");
-        scanf("%s",b[i].flag);
-    }
-    d=i;
-    printf("****menu*****");
-    printf("\n1. Add book information\n2. Display book information\n3. List all books of given author\n4. list the title of specified book\n5. List the count of books in the library\n6. List the books in the order of accession number\n 7.Exit\n");
-    printf("Choose an option\n");
+        scanf("%s",b[i].flag);    }                                                  //here
+
+    d=i;*/
+    int c;
+
+    char v[10] = "Menu";
+
+    printf("%50s\n", v);
+
+    //add();
+
+    printf("1. Add book information\n"
+           "2. Display book information\n"
+           "3. List all books of given author\n"
+           "4. list the title of specified book\n"
+           "5. List the count of books in the library\n"
+           "6. List the books in the order of accession number\n"
+           "7. Exit\n"
+           "Choose an option\n");
     scanf("%d",&c);
     while(c!=7)
     {
-        if(c==1)
-            add();
-        if(c==2)
-            display();
+        if(c==1) add();
+        if(c==2) display();
         if(c==3) author();
         if(c==4) title();
         if(c==5) count();
         if(c==6) access();
         if(c!=1 && c!=2 && c!=3 && c!=4 && c!=5 && c!=6)
             printf("Invalid Input\nPlease enter valid input\n");
+
         printf("1. Add book information\n2. Display book information\n3. List all books of given author\n4. list the title of specified book\n5. List the count of books in the library\n6. List the books in the order of accession number\n 7.Exit\n");
         printf("Choose an option\n");
         scanf("%d",&c);
     }
 }
+
 void add()
 {
-    int i,n;
+    int i, n;
     printf("Enter number of book added\n");
     scanf("%d",&n);
     for(i=d; i<n+d; i++)
     {
-        printf("Enter information of additional book no. %d\n",i-d+1);
+        printf("Enter information of additional book no. %d\n",i-d+1); //here
         printf("Accession number ");
         scanf("%d",&b[i].a);
-        printf(" Name of author ");
+        printf("Name of author ");
         scanf("%s",b[i].name);
         printf("Title of book ");
         scanf("%s",b[i].title);
@@ -77,7 +88,7 @@ void add()
 }
 void display()
 {
-    printf("Accession number  Name      title     price   issue status\n");
+    printf("Accession number  Name      title     price   issue status\n"); //here
     for(int j=0; j<d; j++)
         printf("%d        %s        %s       %f    %s\n",b[j].a,b[j].name,b[j].title,b[j].price,b[j].flag);
 }
@@ -114,7 +125,7 @@ void count()
 {
     printf("no. of books in libarary is %d\n",d);
 }
-void access()
+void access()   //here
 {
     int i,j,E[100],c[100],a;
     for(i=0; i<d; i++)
@@ -137,4 +148,6 @@ void access()
     for(j=0; j<d; j++)
         printf("%d        %s        %s       %f    %s\n",b[c[j]].a,b[c[j]].name,b[c[j]].title,b[c[j]].price,b[c[j]].flag);
 }
+
+
 
